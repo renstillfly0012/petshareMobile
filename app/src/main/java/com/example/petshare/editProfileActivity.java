6f678fcb5ac76656d20b3ccc987fb4eddd6fbd7f
renstillfly0012 <Github.01>
2020-11-04 13:02:27 +0800

    added edit profile layout

---
+++ app/src/main/java/com/example/petshare/editProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.petshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
